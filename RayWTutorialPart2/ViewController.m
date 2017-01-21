44c83a72e2ee56e62a26a57544448fb9212888e2
Suziewan <angielinton@me.com>
2017-01-21 14:55:54 -0500

    First commit

--- RayWTutorialPart2/ViewController.m
+++ RayWTutorialPart2/ViewController.m
@@ -19,11 +19,14 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)buttonTapped:(UIButton *)sender
+{
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+        [sender setTitle:@"A very long title for this button"
+                forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
 }
-
 
 @end
